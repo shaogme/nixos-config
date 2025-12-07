5c54f6cb9eb3511d14a944a5058f394a7cac26a2
韶光 <admin@gloryouth.com>
2025-12-07 13:30:09 +0800

    Refactor flake.nix to streamline outputs and enhance modularity
    - Simplified the outputs structure by introducing a `mkSystem` function to reduce redundancy in system configurations for 'tohu' and 'hyperv' hosts.
    - Updated the inputs section for clarity and removed unnecessary comments.
    - Improved organization of module imports for better maintainability.

--- flake.nix
+++ flake.nix
@@ -3,9 +3,8 @@
 {
   description = "My NixOS Flake Configuration";
 
-  # 1. Inputs: 定义软件源，类似于 Cargo.toml 中的 [dependencies]
+  # 1. Inputs: 定义软件源
   inputs = {
-    # 官方 NixOS 仓库
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
@@ -13,41 +12,46 @@
   };
 
   # 2. Outputs: 定义构建产物
-  outputs = 
-  inputs@{ self, nixpkgs, disko, nixos-facter-modules, ... }: {
-    nixosConfigurations = {
-      tohu = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        
-        # 将 inputs 传递给模块，这样在 configuration.nix 中可以使用 inputs.nixpkgs 等
-        specialArgs = { 
-          inherit inputs;
-          inherit disko;
-          inherit nixos-facter-modules;
-        };
-        
+  outputs = { self, nixpkgs, disko, nixos-facter-modules, ... }@inputs:
+    let
+      commonArgs = {
+        inherit inputs;
+        inherit disko;
+        inherit nixos-facter-modules;
+      };
+
+      # 这个函数接收两个参数：
+      # 1. system: 架构字符串 (如 "x86_64-linux" 或 "aarch64-linux")
+      # 2. extraModules: 模块列表
+      mkSystem = system: extraModules: nixpkgs.lib.nixosSystem {
+        inherit system; # 使用传入的 system 参数
+        specialArgs = commonArgs;
         modules = [
+          # 基础模块
+          ./disk/auto-resize.nix
+        ] ++ extraModules;
+      };
+    in
+    {
+      nixosConfigurations = {
+        
+        # x86_64 机器
+        tohu = mkSystem "x86_64-linux" [
           ./server/vps/hosts/tohu.nix
           ./disk/vps/Swap-2G.nix
-          ./disk/auto-resize.nix
         ];
-      };
-      hyperv = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        
-        # 将 inputs 传递给模块，这样在 configuration.nix 中可以使用 inputs.nixpkgs 等
-        specialArgs = { 
-          inherit inputs;
-          inherit disko;
-          inherit nixos-facter-modules;
-        };
-        
-        modules = [
+
+        hyperv = mkSystem "x86_64-linux" [
           ./server/vps/hosts/hyperv.nix
           ./disk/vps/Swap-4G.nix
-          ./disk/auto-resize.nix
         ];
+        
+        # ARM 机器
+        # raspi = mkSystem "aarch64-linux" [
+        #   ./server/pi/hosts/raspi.nix
+        #   ./disk/pi/sd-card.nix
+        # ];
+
       };
     };
-  };
 }
